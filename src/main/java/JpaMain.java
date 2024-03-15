b27e1105c04f15404ffef58cfa6d80510d0c4a4f
김다은 <sss3598@gmail.com>
2024-03-15 21:37:54 +0900

    JPQL 상속관계 type 사용

--- src/main/java/JpaMain.java
+++ src/main/java/JpaMain.java
@@ -2,6 +2,8 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import jpabook.jpashop.domain.Book;
+import jpabook.jpashop.domain.Item;
 
 public class JpaMain {
 
@@ -13,6 +15,14 @@ public class JpaMain {
     tx.begin();
 
     try {
+
+      Book book = new Book();
+      book.setName("jpa");
+      book.setAuthor("daeun");
+      em.persist(book);
+
+      em.createQuery("select i from Item i where type(i) = Book", Item.class)
+          .getResultList();
 
       tx.commit();
     } catch (Exception e) {
--- src/main/java/jpabook/jpashop/domain/Item.java
+++ src/main/java/jpabook/jpashop/domain/Item.java
@@ -10,10 +10,13 @@ import jakarta.persistence.InheritanceType;
 import jakarta.persistence.ManyToMany;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn
+@Getter @Setter
 public abstract class Item extends BaseEntity {
 
   @Id @GeneratedValue
